c27c7b3bfc33c1f328a302ecad7876131cfe7046
André Bauer <monotek23@gmail.com>
2018-06-08 00:28:47 +0200

    added real username to log message

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,11 @@ FTP_PASS="${FTP_PASS:=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c10)}"
 FTP_USER="${FTP_USER:=ftpuser}"
 FTP_TIMEOUT="${FTP_TIMEOUT:=infinity}"
 FTP_PORT_CONTROL="${FTP_PORT_CONTROL:=21}"
-FTP_PORTS_DATA="${FTP_PORTS_DATA}"
 
 echo -e "Creating user ${FTP_USER} with home dir ${FTP_DIR}\n"
 useradd -m -d ${FTP_DIR} -k /noskel ${FTP_USER} -s /bin/bash
 
-echo -e "Set user password to ${FTP_PASS}\n"
+echo -e "Set ${FTP_USER} password to ${FTP_PASS}\n"
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
 echo -e "Changing proftpd.conf\n"
