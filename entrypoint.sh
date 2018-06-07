0f3919c00289eedc775045eb595a6cef10737eb4
André Bauer <monotek23@gmail.com>
2018-06-08 00:17:21 +0200

    splittet ports to 2 vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ FTP_DIR="${FTP_DIR:=/ftp}"
 FTP_PASS="${FTP_PASS:=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c10)}"
 FTP_USER="${FTP_USER:=ftpuser}"
 FTP_TIMEOUT="${FTP_TIMEOUT:=infinity}"
-FTP_PORT_CONTROL=${FTP_PORT_CONTROL:=21}
-FTP_PORTS_DATA=${FTP_PORTS_DATA}
+FTP_PORT_CONTROL="${FTP_PORT_CONTROL:=21}"
+FTP_PORTS_DATA="${FTP_PORTS_DATA}"
 
 echo -e "Creating user ${FTP_USER} with home dir ${FTP_DIR}\n"
 useradd -m -d ${FTP_DIR} -k /noskel ${FTP_USER} -s /bin/bash
@@ -19,7 +19,7 @@ echo -e "Changing proftpd.conf\n"
 sed -i /etc/proftpd/proftpd.conf -e 's/# DefaultRoot/DefaultRoot/' -e "s/Port\s*21/Port ${FTP_PORT_CONTROL}/"
 
 if [ -n "${FTP_PORTS_DATA}" ]; then
-  sed -i /etc/proftpd/proftpd.conf -e "s/# PassivePorts.*/PassivePorts ${FTP_PORTS_DATA}/"
+  sed -i /etc/proftpd/proftpd.conf -e "s/# PassivePorts.*/PassivePorts ${FTP_PORTS_DATA_BEG} ${FTP_PORTS_DATA_END}/"
 fi
 
 echo -e "Starting ftp service on port 8021... will exit in ${FTP_TIMEOUT} seconds... \n"
