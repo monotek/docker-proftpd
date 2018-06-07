469d2243f62c9f3d1d95373bb24a933ad0a9c43e
André Bauer <monotek23@gmail.com>
2018-06-08 01:03:03 +0200

    using backreference

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,14 +15,13 @@ echo -e "Set ${FTP_USER} password to ${FTP_PASS}\n"
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
 echo -e "Changing proftpd.conf\n"
-sed -i /etc/proftpd/proftpd.conf -e 's/# DefaultRoot/DefaultRoot/' -e "s/Port\s*21/Port ${FTP_PORT_CONTROL}/" -e 's#/var/log.*#/dev/stdout#g'
+sed -i /etc/proftpd/proftpd.conf -e 's/# DefaultRoot/DefaultRoot/' -e "s/Port\s*21/Port ${FTP_PORT_CONTROL}/" -e 's#\(TransferLog\|SystemLog\)#\\1 /dev/stdout#g'
 
 if [ -n "${FTP_PORTS_DATA_BEG}" ] && [ -n "${FTP_PORTS_DATA_END}" ]; then
   sed -i /etc/proftpd/proftpd.conf -e "s/# PassivePorts.*/PassivePorts ${FTP_PORTS_DATA_BEG} ${FTP_PORTS_DATA_END}/"
 fi
 
 echo -e "Fixing directory rights\n"
-chmod o+w /dev/stdout
 chown -R ${FTP_USER}:${FTP_USER} ${FTP_DIR}
 
 echo -e "Starting ftp service on port 8021... will exit in ${FTP_TIMEOUT} seconds... \n"
@@ -33,3 +32,5 @@ while true; do
   echo -e "\n Timeout reached... Stopping ftp process but keep container alive... \n"
   service proftpd stop
 done
+TransferLog /var/log/proftpd/xferlog
+SystemLog   /var/log/proftpd/proftpd.log
