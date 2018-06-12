c6570f0968cb83b245b64b70364a2486bfe520a1
André Bauer <andre.bauer@kiwigrid.com>
2018-06-12 16:19:06 +0200

    added MasqueradeAddress

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ FTP_DIR="${FTP_DIR:=/ftp}"
 FTP_PASS="${FTP_PASS:=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c10)}"
 FTP_USER="${FTP_USER:=ftpuser}"
 FTP_TIMEOUT="${FTP_TIMEOUT:=infinity}"
-FTP_PORT_CONTROL=${FTP_PORT_CONTROL:=21}
-FTP_PORTS_DATA=${FTP_PORTS_DATA}
+FTP_PORT_CONTROL="${FTP_PORT_CONTROL:=21}"
+FTP_PORTS_DATA="${FTP_PORTS_DATA}"
+FTP_MASQUERADEADDRESS="${FTP_MASQUERADEADDRESS}"
 
 echo -e "Creating user ${FTP_USER} with home dir ${FTP_DIR}\n"
 useradd -m -d ${FTP_DIR} -k /noskel ${FTP_USER} -s /bin/bash
@@ -17,6 +18,10 @@ echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
 echo -e "Changing proftpd.conf\n"
 sed -i /etc/proftpd/proftpd.conf -e 's/# DefaultRoot/DefaultRoot/' -e "s/Port\s*21/Port ${FTP_PORT_CONTROL}/"
+
+if [ -n "${FTP_MASQUERADEADDRESS}" ]; then
+  sed -i /etc/proftpd/proftpd.conf -e "s/# MasqueradeAddress.*/MasqueradeAddress ${FTP_MASQUERADEADDRESS}/"
+fi
 
 if [ -n "${FTP_PORTS_DATA}" ]; then
   sed -i /etc/proftpd/proftpd.conf -e "s/# PassivePorts.*/PassivePorts ${FTP_PORTS_DATA}/"
